15513f78ac8e8d05487c0dd35ac6969a526418b8
ruviRedner <r.y.r7966@gmail.com>
2024-09-08 10:11:08 +0300

    playing with elements

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-window.prompt("Do you think is ruby the best?")
+const title = document.createElement("h2")
+
+title.textContent = "hola ruby"
+const inp = document.createElement("input")
+inp.type = "range"
+inp.min = 0
+inp.max = 100
+inp.value = 20
+inp.step = 10
+let prev = 20
+
+
+inp.addEventListener("change" , () => {
+    if(inp.value > prev){
+        title.textContent = inp.value
+    }
+    prev = inp.value
+    
+
+})
+document.body.appendChild(title)
+document.body.appendChild(inp)
+
+
